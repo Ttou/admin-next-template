00b489a0b148757d4ce85f1f6a77d44f1d11e57d
WangRuiJin <wangruijin@mrfashion.com>
2022-02-17 09:40:24 +0800

    refactor: 移除测试框架

--- vite.config.js
+++ vite.config.js
@@ -71,11 +71,5 @@ export default defineConfig({
   server: {
     host: true,
     port: 3000
-  },
-  test: {
-    environment: 'happy-dom',
-    transformMode: {
-      web: [/\.[jt]sx$/]
-    }
   }
 })
