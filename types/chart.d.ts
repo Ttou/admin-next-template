6c2c7c9c56a36c43d2278940c8370a2b9def8b3b
wangruijin <1361572192@qq.com>
2023-01-13 14:44:00 +0800

    fix(custom): 修复 echarts 类型

--- types/chart.d.ts
+++ types/chart.d.ts
@@ -1,3 +1,13 @@
-declare type ChartProps = import('vue').ExtractPropTypes<
-  typeof import('vue-echarts')['default']['props']
-> & { option: import('echarts').EChartsOption }
+type EChartsOption = import('echarts').EChartsOption
+type SetOptionOpts = import('echarts').SetOptionOpts
+type VueChartInstance = InstanceType<typeof import('vue-echarts')['default']>
+type ExtractPropTypes<T> = import('vue').ExtractPropTypes<T>
+
+declare type ChartProps = Omit<
+  ExtractPropTypes<VueChartInstance>,
+  '$el' | 'option'
+> & { option?: EChartsOption }
+
+declare type ChartRef = Omit<VueChartInstance, 'setOption'> & {
+  setOption: (option: EChartsOption, updateOptions?: SetOptionOpts) => void
+}
