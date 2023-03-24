8e7d4981a6c24d6aeed6f6b66a2e1cf715aa198e
wangruijin <1361572192@qq.com>
2023-03-24 15:56:22 +0800

    refactor(custom): 完善代码

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,13 @@
 import vue from '@vitejs/plugin-vue'
-import ejs from 'ejs'
 import { resolve } from 'path'
 import elementPlus from 'unplugin-element-plus/vite'
-import { defineConfig, loadEnv, PluginOption } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import { compression } from 'vite-plugin-compression2'
 import eslint from 'vite-plugin-eslint2'
 import { viteMockServe } from 'vite-plugin-mock'
 import stylelint from 'vite-plugin-stylelint'
 
-/**
- * index.html 处理插件
- * @param options 写法参照 ejs 文档
- * @see https://ejs.co/#docs
- */
-function processIndexHtml(options: Record<string, any>): PluginOption {
-  return {
-    name: 'self:processHtml',
-    enforce: 'pre',
-    transformIndexHtml: {
-      order: 'pre',
-      handler(html) {
-        return ejs.render(html, { ...options }, { root: __dirname })
-      }
-    }
-  }
-}
+import { customHtml } from './vite-plugin-html'
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.')
@@ -53,7 +36,7 @@ export default defineConfig(({ mode }) => {
       stylelint({
         lintInWorker: true
       }),
-      processIndexHtml({
+      customHtml({
         injectVer: `<meta name="version-no" content="${new Date().getTime()}"/>`,
         injectTitle: `<title>${env.VITE_APP_TITLE}</title>`
       }),
