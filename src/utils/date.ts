d7b24960e5c85b30f69eb68c9febf607876bc457
Ttou <1361572192@qq.com>
2023-08-29 11:46:27 +0800

    refactor(custom): 完善代码

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -6,25 +6,27 @@ dayjs.locale('zh-cn')
 
 export { dayjs }
 
+type IDate = string | number | Date | dayjs.Dayjs
+
 /**
  * 格式化日期
  * @param value
  * @param fmt
  */
-export function fmtDate(value, fmt = 'YYYY-MM-DD HH:mm:ss') {
+export function fmtDate(value: IDate, fmt = 'YYYY-MM-DD HH:mm:ss') {
   return dayjs(value).format(fmt)
 }
 
 /**
  * 每天开始时间
  */
-export function startOfDay(value) {
+export function startOfDay(value: IDate) {
   return dayjs(value).startOf('day').toDate()
 }
 
 /**
  * 每天结束时间
  */
-export function endOfDay(value) {
+export function endOfDay(value: IDate) {
   return dayjs(value).endOf('day').toDate()
 }
