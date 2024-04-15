c03edd80fb3b013ad529f8757d4221a5d62a1150
wangruijin <1361572192@qq.com>
2024-04-15 10:18:53 +0800

    style(custom): 格式化

--- src/constants/cssVars.ts
+++ src/constants/cssVars.ts
@@ -1,5 +1,5 @@
 export const CSS_VARS = {
-  headerHeight: ' var(--header-height)',
+  headerHeight: 'var(--header-height)',
   menuWidth: 'var(--menu-width)',
   menuCollapseWidth: 'var(--menu-collapse-width)',
   menuBg: 'var(--menu-bg)',
