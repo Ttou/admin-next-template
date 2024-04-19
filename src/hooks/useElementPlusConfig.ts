11e160bc59643f4820403dd82d56e2ceb5886953
wangruijin <1361572192@qq.com>
2024-04-19 23:22:06 +0800

    build(custom): 更新依赖

--- src/hooks/useElementPlusConfig.ts
+++ src/hooks/useElementPlusConfig.ts
@@ -1,6 +1,5 @@
 import { type ConfigProviderProps } from 'element-plus'
-// @ts-ignore
-import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
 
 export function useElementPlusConfig(): Partial<ConfigProviderProps> {
   return {
